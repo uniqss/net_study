2c3f01d4c84a64c6833033c55a4bced2020cd879
uniqs <uniqs@163.com>
2023-05-14 17:46:31 +0000

    fix the core dump problem.

--- src/echo_client_bench20.cpp
+++ src/echo_client_bench20.cpp
@@ -25,15 +25,16 @@ enum { max_length = 1024 };
 int max_client_count = 10000;
 int ok_count = 0;
 int64_t ok_count_total = 0;
+bool gworking = false;
 
 void setdebugging() {
-    if (max_client_count <= 2) goDebugging = true;
+    if (max_client_count <= 10) goDebugging = true;
 }
 
 awaitable<void> print_proc() {
     auto executor = co_await this_coro::executor;
     int ok_count_0 = 0;
-    for (;;) {
+    for (; gworking;) {
         asio::steady_timer timer(executor);
         timer.expires_after(std::chrono::seconds(1));
         co_await timer.async_wait(use_awaitable);
@@ -53,51 +54,31 @@ awaitable<void> print_proc() {
     }
 }
 
-class client : public std::enable_shared_from_this<client> {
+class IClient {
    public:
-    client(int cidx, asio::io_context& ctx, const std::string& host, const std::string& port)
-        : m_ctx(ctx), m_cidx(cidx), m_skt(ctx), m_host(host), m_port(port) {
+    virtual ~IClient() {}
+    virtual void start() = 0;
+    virtual void stop() = 0;
+};
+
+using clientPtr = std::shared_ptr<IClient>;
+std::unordered_map<int, clientPtr> gClients;
+
+class client : public IClient, public std::enable_shared_from_this<client> {
+   public:
+    client(int cidx, asio::io_context& ctx, const auto& host, const auto& port)
+        : m_ctx(ctx), m_cidx(cidx), m_skt(ctx), m_host(host), m_port(port), m_reading(false), m_writing(false) {
         std::memset(m_buffRequest, 'a' + ((cidx) % 26), max_length);
         m_buffRequest[max_length - 1] = '\0';
     }
+    ~client() { dlog("~client cidx:", m_cidx); }
 
-    awaitable<void> client_proc_connect() {
+    awaitable<void> do_read() {
+        if (m_reading) co_return;
+        m_reading = true;
         auto executor = m_skt.get_executor();
         try {
-            for (;;) {
-                if (!m_skt.is_open()) {
-                    tcp::resolver resolver(executor);
-                    asio::error_code ec;
-                    asio::connect(m_skt, resolver.resolve(m_host, m_port), ec);
-                    ilog("open and connecting cidx:", m_cidx, "ec:", ec.value());
-                    if (!ec && m_skt.is_open()) {
-                        co_spawn(
-                            m_skt.get_executor(), [self = shared_from_this()] { return self->client_proc_read(); },
-                            detached);
-                        co_spawn(
-                            m_skt.get_executor(), [self = shared_from_this()] { return self->client_proc_write(); },
-                            detached);
-                    }
-                } else {
-                }
-                asio::steady_timer timer(executor);
-                timer.expires_after(std::chrono::seconds(1));
-                co_await timer.async_wait(use_awaitable);
-            }
-        } catch (const asio::system_error& ec) {
-            if (ec.code() == asio::error::connection_refused) {
-            }
-            elog("client_proc_connect asio::system_error error:", ec.code(), " error.msg:", ec.what());
-        } catch (const std::exception& e) {
-            elog("client_proc_connect error:", e.what());
-            m_skt.close();
-        }
-    }
-
-    awaitable<void> client_proc_read() {
-        auto executor = m_skt.get_executor();
-        try {
-            for (;;) {
+            for (; gworking;) {
                 if (m_skt.is_open()) {
                     dlog("pre async_read cidx:", m_cidx);
                     size_t reply_length =
@@ -112,44 +93,58 @@ class client : public std::enable_shared_from_this<client> {
                         // dlog("everything is ok cidx:", m_cidx);
                         ++ok_count;
                     }
+                } else {
+                    dlog("do_read m_skt is not open cidx:", m_cidx);
                 }
                 asio::steady_timer timer(executor);
                 timer.expires_after(std::chrono::seconds(1));
                 co_await timer.async_wait(use_awaitable);
             }
+            m_reading = false;
         } catch (const std::exception& e) {
-            elog("client_proc_read error:", e.what());
+            elog("do_read error:", e.what());
             m_skt.close();
+            m_reading = false;
         }
     }
 
-    awaitable<void> client_proc_write() {
+    awaitable<void> do_write() {
+        if (m_writing) co_return;
+        m_writing = true;
         auto executor = m_skt.get_executor();
         try {
-            for (;;) {
+            for (; gworking;) {
                 if (m_skt.is_open()) {
+                    dlog("do_write 111 cidx:", m_cidx);
                     if (!m_writemsgs.empty()) {
                         std::string msg = m_writemsgs.front();
                         m_writemsgs.pop_front();
                         dlog("pre async_write cidx:", m_cidx);
                         co_await asio::async_write(m_skt, asio::buffer(msg.data(), max_length), use_awaitable);
                         dlog("post async_write cidx:", m_cidx);
+                    } else {
+                        dlog("m_writemsgs.empty() cidx:", m_cidx);
                     }
+                } else {
+                    dlog("do_write m_skt is not open cidx:", m_cidx);
                 }
                 asio::steady_timer timer(executor);
                 timer.expires_after(std::chrono::seconds(1));
                 co_await timer.async_wait(use_awaitable);
             }
+            m_writing = false;
         } catch (const std::exception& e) {
-            elog("client_proc_write error:", e.what());
+            elog("do_write error:", e.what());
             m_skt.close();
+            m_writing = false;
         }
     }
 
     awaitable<void> addmsg_forsend() {
         auto executor = m_skt.get_executor();
         try {
-            for (;;) {
+            for (; gworking;) {
+                dlog("addmsg_forsend 111 cidx:", m_cidx);
                 if (m_writemsgs.size() < 2) m_writemsgs.push_back(m_buffRequest);
 
                 asio::steady_timer timer(executor);
@@ -161,60 +156,47 @@ class client : public std::enable_shared_from_this<client> {
         }
     }
 
-    // 4 coroutines
-    awaitable<void> client_proc() {
+    awaitable<void> do_connect() {
         auto executor = m_skt.get_executor();
         try {
-            for (;;) {
+            for (; gworking;) {
                 if (!m_skt.is_open()) {
-                    co_spawn(
-                        m_skt.get_executor(), [self = shared_from_this()] { return self->client_proc_connect(); },
-                        detached);
-                }
-                asio::steady_timer timer(executor);
-                timer.expires_after(std::chrono::seconds(1));
-                co_await timer.async_wait(use_awaitable);
-            }
-        } catch (const std::exception& e) {
-            elog("client_proc error:", e.what());
-        }
-    }
-
-    // 3 coroutines
-    awaitable<void> client_proc_c3() {
-        auto executor = m_skt.get_executor();
-        try {
-            for (;;) {
-                if (!m_skt.is_open()) {
-                    tcp::resolver resolver(executor);
-                    asio::error_code ec;
-                    asio::connect(m_skt, resolver.resolve(m_host, m_port), ec);
-                    ilog("open and connecting cidx:", m_cidx, " ec:", ec.value(), "|", ec.message());
-                    if (!ec && m_skt.is_open()) {
+                    m_ec.clear();
+                    m_skt.connect(asio::ip::tcp::endpoint(asio::ip::address::from_string(m_host), m_port), m_ec);
+                    ilog("open and connecting cidx:", m_cidx, " ec:", m_ec.value(), "|", m_ec.message());
+                    if (!m_ec) {
                         co_spawn(
-                            m_skt.get_executor(), [self = shared_from_this()] { return self->client_proc_read(); },
-                            detached);
+                            m_skt.get_executor(), [self = shared_from_this()] { return self->do_read(); }, detached);
                         co_spawn(
-                            m_skt.get_executor(), [self = shared_from_this()] { return self->client_proc_write(); },
-                            detached);
+                            m_skt.get_executor(), [self = shared_from_this()] { return self->do_write(); }, detached);
+                    } else {
+                        m_skt.close();
                     }
+                } else {
+                    dlog("do_connect m_skt is open cidx:", m_cidx);
                 }
                 asio::steady_timer timer(executor);
-                timer.expires_after(std::chrono::seconds(1));
+                timer.expires_after(std::chrono::seconds(5));
                 co_await timer.async_wait(use_awaitable);
             }
         } catch (const std::exception& e) {
-            elog("client_proc_c3 error:", e.what());
+            elog("connector error:", e.what());
         }
     }
 
-    void start() {
+    virtual void start() {
+        gClients[m_cidx] = shared_from_this();
         co_spawn(
-            m_skt.get_executor(), [self = shared_from_this()] { return self->client_proc_c3(); }, detached);
+            m_skt.get_executor(), [self = shared_from_this()] { return self->do_connect(); }, detached);
         co_spawn(
             m_skt.get_executor(), [self = shared_from_this()] { return self->addmsg_forsend(); }, detached);
 
         ilog("client_proc exiting cidx:", m_cidx);
+    }
+    virtual void stop() {
+        if (m_skt.is_open()) {
+            m_skt.close();
+        }
     }
 
    private:
@@ -222,36 +204,45 @@ class client : public std::enable_shared_from_this<client> {
     int m_cidx;
     tcp::socket m_skt;
     std::string m_host;
-    std::string m_port;
+    uint16_t m_port;
     char m_buffRequest[max_length];
     char m_buffReply[max_length];
     std::deque<std::string> m_writemsgs;
+    asio::error_code m_ec;
+    bool m_reading;
+    bool m_writing;
 };
-
-using clientPtr = std::shared_ptr<client>;
-std::unordered_map<int, clientPtr> gClients;
 
 int main(int argc, char* argv[]) {
     try {
         std::string host = "127.0.0.1";
-        std::string port = "8888";
+        uint16_t port = 8888;
         if (argc >= 4) {
             host = argv[1];
-            port = argv[2];
+            port = (uint16_t)atoi(argv[2]);
             max_client_count = atoi(argv[3]);
             if (max_client_count < 2) max_client_count = 2;
         }
 
         setdebugging();
 
+        gworking = true;
+
         asio::io_context io_context(1);
 
         asio::signal_set signals(io_context, SIGINT, SIGTERM);
-        signals.async_wait([&](auto, auto) { io_context.stop(); });
+        signals.async_wait([&](auto, auto) {
+            gworking = false;
+            for (auto c : gClients) {
+                c.second->stop();
+            }
+
+            io_context.stop();
+        });
 
         for (int i = 1; i <= max_client_count; ++i) {
-            gClients[i] = std::make_shared<client>(i, io_context, host, port);
-            gClients[i]->start();
+            auto c = std::make_shared<client>(i, io_context, host, port);
+            c->start();
         }
 
         co_spawn(io_context, print_proc(), detached);
--- src/simple_log.h
+++ src/simple_log.h
@@ -8,18 +8,18 @@ template <typename... ARGS>
 void dlog(ARGS&&... args) {
     if (goDebugging) {
         ((std::cout << args), ...);
-        std::cout << std::endl;
+        std::cout << "\n";
     }
 }
 
 template <typename... ARGS>
 void ilog(ARGS&&... args) {
     ((std::cout << args), ...);
-    std::cout << std::endl;
+    std::cout << "\n";
 }
 
 template <typename... ARGS>
 void elog(ARGS&&... args) {
     ((std::cout << args), ...);
-    std::cout << std::endl;
+    std::cout << "\n";
 }
