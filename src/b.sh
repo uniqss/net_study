bcf4b6157786673b23b77aa6f366016972eea807
uniqs <uniqs@163.com>
2023-05-15 02:28:37 +0000

    build shell use relative path

--- src/b.sh
+++ src/b.sh
@@ -2,4 +2,4 @@ fname=$( echo "$1" | cut -d "." -f1 )
 
 echo building $fname.cpp  "=>" ./bin/$fname
 
-g++ -g -Wall -std=c++2a -O0 -I/home/uniqs/asio/asio-1.28.0/include/ $fname.cpp -o ./bin/$fname
+g++ -g -Wall -std=c++2a -O0 -I../opt/asio-1.28.0/include/ $fname.cpp -o ./bin/$fname
